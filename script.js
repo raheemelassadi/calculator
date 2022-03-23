71fcaa72bc8fdc7fa792691c6287b38dd7b28182
raheemelassadi <raheemelassadi1@gmail.com>
2022-03-23 13:53:23 -0700

    some bug fixes and fixed comments

--- script.js
+++ script.js
@@ -1,30 +1,34 @@
+//adds inputs
 function add (a, b){
     return a + b;
 }
-
+//subtracts inputs
 function subract (a, b){
     return a - b ;
 }
-
+//mulitplies inputs
 function multiply (a, b){
     return a * b;
 }
-
+//divides inputs
 function divide (a, b){
     return a / b;
 }
-
+//runs the operaters with the inputs
 function operate (func, a, b){
     return func(a, b);
 }
-
+//selects each button 
 const selectButtons = document.querySelectorAll('button');
-
+//selects the display screen
 const screen = document.querySelector('.screen');
+
+//if button value = AC then reset display value to nothing ('')
+
 
 
 for (let i = 0; i < selectButtons.length; i++){
-    selectButtons[i].addEventListener('click', function(event){
+    selectButtons[i].addEventListener('click', function(){
         const currVal = selectButtons[i].firstChild.nodeValue;
         parseInt(currVal);
         const displayValue = document.createElement('p');
